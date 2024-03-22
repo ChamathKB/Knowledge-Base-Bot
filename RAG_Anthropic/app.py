be5bbf9be9bfd7b7c69cbdb274f76675b1dad431
ChamathKB <chamathkbatta@gmail.com>
2024-03-22 23:36:30 +0530

    add vectordb and embedding components to anthropic demo

--- RAG_Anthropic/app.py
+++ RAG_Anthropic/app.py
@@ -1,4 +1,10 @@
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import FAISS
+
 import streamlit as st
 import pickle
 import os
@@ -13,6 +19,9 @@ DOCS_DIR = os.path.abspath("./uploaded_docs")
 
 
 anthropic = Anthropic(api_key=API_KEY)
+document_embedder = NVIDIAEmbeddings(model="nvolveqa_40k", model_type="passage")
+query_embedder = NVIDIAEmbeddings(model="nvolveqa_40k", model_type="query")
+
 
 def chat(user_input):
     stream = anthropic.completions.create(
@@ -39,3 +48,37 @@ def document_loader(DOCS_DIR):
             with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
                 f.write(uploaded_file.read())
 
+
+with st.sidebar:
+    use_existing_vector_store = st.radio("Use existing vector store if available", ["Yes", "No"], horizontal=True)
+
+
+vector_store_path = "vectorstore.pkl"
+
+
+raw_documents = DirectoryLoader(DOCS_DIR).load()
+
+
+vector_store_exists = os.path.exists(vector_store_path)
+vectorstore = None
+if use_existing_vector_store == "Yes" and vector_store_exists:
+    with open(vector_store_path, "rb") as f:
+        vectorstore = pickle.load(f)
+    with st.sidebar:
+        st.success("Existing vector store loaded successfully.")
+else:
+    with st.sidebar:
+        if raw_documents:
+            with st.spinner("Splitting documents into chunks..."):
+                text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+                documents = text_splitter.split_documents(raw_documents)
+
+            with st.spinner("Adding document chunks to vector database..."):
+                vectorstore = FAISS.from_documents(documents, document_embedder)
+
+            with st.spinner("Saving vector store"):
+                with open(vector_store_path, "wb") as f:
+                    pickle.dump(vectorstore, f)
+            st.success("Vector store created and saved.")
+        else:
+            st.warning("No documents available to process!", icon="⚠️")
